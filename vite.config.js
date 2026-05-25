394dcf88f0563fa13e2f40860a992fd930c770ff
nithyasriarjava <125264594+nithyasriarjava@users.noreply.github.com>
2026-05-25 19:45:45 +0530

    config

--- vite.config.js
+++ vite.config.js
@@ -6,15 +6,17 @@ import { defineConfig } from 'vite';
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
+    base: '/career-guidance-app/', // GitHub repo name
+
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
       },
     },
+
     server: {
       hmr: process.env.DISABLE_HMR !== 'true',
       watch: process.env.DISABLE_HMR === 'true' ? null : {},
     },
   };
 });
-
